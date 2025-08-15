0e8aab1fbfbd4a76f98ae594ec7681a19d4553f0
Omkar Nerurkar <omkarnerurkar7@gmail.com>
2025-08-15 20:10:53 +0530

    Added a functionable button for upper to lowercase with help of state and props

--- src/protoTYpes/TextForm.js
+++ src/protoTYpes/TextForm.js
@@ -13,15 +13,28 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
+    const handleDwnClick = ()=>{
+        console.log( "down handle is clicked" );
+        const nextText = text.toLowerCase();
+        setText( nextText );
+    }
+
     const [text, setText] = useState("Enter your text");
   return (
-    <div>
-      
+    <>
+     <div className="container">
         <div className="mb-3">
             <h1>{props.headings}</h1>
             <textarea className="form-control my-3" onChange={handleOnChange} value={text} id="exampleFormControlTextarea1" rows="5"></textarea>
         </div>
         <button type="button" className="btn btn-primary" onClick={handleUpClick}>UpperCase</button>
+        <button type="button" className="btn btn-success mx-3" onClick={handleDwnClick}>LowerCase</button>
     </div>
+    <div className="container my-4">
+        <h1>Your text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length } Minutes read</p>
+    </div>
+    </>
   )
 }
